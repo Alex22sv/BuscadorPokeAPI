3c26a42f913b8cf2cdc20041202716c7eede69ca
Alex22sv <ae22mp@gmail.com>
2024-09-22 18:40:45 -0600

    Update class and ID names

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 
 
 window.addEventListener("load", () => {
-    const form = document.querySelector("#new-poke-form");
-    const input = document.querySelector("#new-poke-input");
+    const form = document.querySelector("#new_pokemon_form");
+    const input = document.querySelector("#new_pokemon_input");
     const list_el = document.querySelector("#pokemons");
     const header = document.querySelector("header");
 
@@ -28,7 +28,7 @@ window.addEventListener("load", () => {
             return;
         }
         const poke_el = document.createElement("div");
-        poke_el.classList.add("poke");
+        poke_el.classList.add("pokemon");
 
         const poke_content_el = document.createElement("div");
         poke_content_el.classList.add("content");
